a100b67d21dc6c0ee323ae188af857b0b7eeeb55
sxuan0217 <sxuan0217@gmail.com>
2017-07-10 15:13:18 +0800

    ajax
    ajax

---
+++ js/custom.js
@@ -0,0 +1,75 @@
+var xhr = new XMLHttpRequest();
+xhr.open('get', 'http://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97', true);
+xhr.send(null);
+var data ='';
+var attractions;
+var attractionLen;
+var btn =document.querySelector('.popular');
+var list = document.querySelector('.list');
+var AreaSearch = document.querySelector('.select');
+var areaH3 =document.querySelector('.areaName');
+xhr.onload = function () {
+ data = (JSON.parse(xhr.responseText));   
+ attractions = data.result.records;
+ attractionLen = data.result.records.length;
+ var allArea = [];
+var selectArea;
+//將allarea的空陣列放入所有地區並排除重複後再放入selectArea中
+function upadateMenu() {
+    for (var i = 0; i < attractionLen; i++) {
+        allArea.push(attractions[i].Zone);
+    }
+    selectArea = allArea.filter(function (el, i, arr) {
+        return arr.indexOf(el) === i;
+    });
+
+}
+upadateMenu();//開啟頁面時，更新一次
+//將selectArea組成字串放入AreaSearch物件中!
+var selectStr = '';
+for (var i = 0; i < selectArea.length; i++) {
+    selectStr += '<option>' + selectArea[i] + '</option>';
+    AreaSearch.innerHTML = '<option>--請選擇行政區--</option>' + selectStr;
+}
+AreaSearch.addEventListener('change',printData);//監聽AreaSearch
+btn.addEventListener('click',printData);//監聽btn
+//列出資料
+function printData(e) {
+        var searchValue = e.target.value;//獲取點擊的物件的value
+        var searchName = e.target.nodeName;//獲取點擊的物件的節點名稱
+        
+    if (searchName !== 'INPUT' && searchName !== 'SELECT') {
+            return;
+        }
+
+    var str = "";
+    for (var i = 0; attractionLen > i; i++) {
+        if (searchValue ==attractions[i].Zone ) {
+        areaH3.textContent = attractions[i].Zone;
+        str += "<li class='countrybox '>";
+        str += '<div class="picture"style="background-image: url(' + attractions[i].Picture1 + ')";></div>';
+        str += '<div class="countryInfo">';
+        str += '<p><img class="iconSet" src="./img/icons_clock.png" alt="">' + attractions[i].Opentime + '</p>';
+        str += '<p><img class="iconSet" src="./img/icons_pin.png" alt="">' + attractions[i].Add + '</p>'
+        str += '<p><img class="iconSet" src="./img/icons_phone.png" alt="">' + attractions[i].Tel + '</p>'
+        str += '<p class="right"><img class="iconSet" src="./img/icons_tag.png" alt="">' + attractions[i].Ticketinfo + '</p>'
+        str += '</div>'
+        str += '</li>';
+         }
+
+
+
+
+    }
+    list.innerHTML = str;
+}
+
+
+}
+
+
+
+
+
+
+
