3447dcc1a268b79fe5c9e541c080a7db82ee2bae
sxuan0217 <sxuan0217@gmail.com>
2018-05-10 17:33:50 +0800

    fix api

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();
-xhr.open('get', 'http://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97', true);
+xhr.open('get', 'https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97', true);
 xhr.send(null);
 var data = '';
 var attractions;
